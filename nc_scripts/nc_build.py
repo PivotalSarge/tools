1194a7a16f0f69f367c6559f99e6bcdad2297efe
Sarge <mdodge@pivotal.io>
2016-12-02 10:26:38 -0800

    Add determination of build directory.

--- nc_scripts/nc_build.py
+++ nc_scripts/nc_build.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import os
+import subprocess
 
-def getRoot():
+def getRootDir():
     dir = os.getcwd()
     while dir and dir != "/":
         if os.path.exists(os.path.join(dir, ".git")):
@@ -10,8 +11,25 @@ def getRoot():
         dir = os.path.dirname(dir)
     return None
 
-gitRoot = getRoot()
-if gitRoot:
-    print("gitRoot=" + gitRoot)
+def getBuildDir(rootDir):
+    if os.path.exists(os.path.join(rootDir, "CMakeCache.txt")):
+        return rootDir
+    for child in os.listdir(rootDir):
+        childDir = os.path.join(rootDir, child)
+        if os.path.exists(os.path.join(childDir, "CMakeCache.txt")):
+            return childDir
+    return None
 
-exit(37)
+gitRootDir = getRootDir()
+if not gitRootDir:
+    print("Unable to determine git root directory")
+    exit(1)
+buildDir = getBuildDir(gitRootDir)
+if not buildDir:
+    print("Unable to determine build directory")
+    exit(1)
+os.chdir(buildDir)
+
+subprocess.call("ls")
+
+exit(0)
