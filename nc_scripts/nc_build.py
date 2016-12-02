1e400b0a04aa4a16df19553da00c8fb3b5c50d69
Sarge <mdodge@pivotal.io>
2016-12-02 09:52:18 -0800

    Add determination of git root.

--- nc_scripts/nc_build.py
+++ nc_scripts/nc_build.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
 
-print("Hello, world!")
+import os
+
+def getRoot():
+    dir = os.getcwd()
+    while dir and dir != "/":
+        if os.path.exists(os.path.join(dir, ".git")):
+            return dir
+        dir = os.path.dirname(dir)
+    return string.empty
+
+gitRoot = getRoot()
+if gitRoot:
+    print("gitRoot={}".format(gitRoot))
+
 exit(37)
