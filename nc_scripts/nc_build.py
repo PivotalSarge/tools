5e50ee55502982507df2eaffbb4c4ec69988852b
Sarge <mdodge@pivotal.io>
2016-12-05 13:27:11 -0800

    Add support for configure and build targets.

--- nc_scripts/nc_build.py
+++ nc_scripts/nc_build.py
@@ -25,6 +25,7 @@ gitRootDir = getRootDir()
 if not gitRootDir:
     print("Unable to determine git root directory")
     exit(1)
+srcDir = os.path.join(gitRootDir, 'src')
 buildDir = getBuildDir(gitRootDir)
 if not buildDir:
     print("Unable to determine build directory")
@@ -32,18 +33,26 @@ if not buildDir:
 os.chdir(buildDir)
 
 parser = argparse.ArgumentParser(description='Build the native client.')
-parser.add_argument('targets', metavar='target', nargs='*', help='a target to build')
+parser.add_argument('targets', metavar='target', nargs='*', help='target to build')
+parser.add_argument('--config', dest='build_type', default='Debug', help='build type')
+parser.add_argument('--install', dest='install_prefix', default='/tmp', help='build type')
 args = parser.parse_args()
+
+gemfire_home=os.environ.get('GEMFIRE_HOME')
+if not gemfire_home:
+    gemfire_home = '/gemfire'
 
 targets = args.targets
 if not targets:
     targets.extend(['build'])
 for target in targets:
-    print target
-    if target == 'build':
-        command = 'cmake --build . --config Debug -- -j 8'
+    if target == 'configure':
+        command = 'cmake -DGEMFIRE_HOME=%s -DCMAKE_BUILD_TYPE=%s -DCMAKE_INSTALL_PREFIX=%s %s' % (gemfire_home, args.build_type, args.install_prefix, srcDir)
+    elif target == 'build':
+        command = 'cmake --build . --config %s -- -j 8' % args.build_type
     else:
-        command = 'cmake --build . --config Debug --target ' + target
+        command = 'cmake --build . --config %s --target %s' % (args.build_type, target)
+    print command
     subprocess.call(command, shell=True)
 
 exit(0)
