2dc45c081327558d733bba8fd661bc06a8992693
Sarge <mdodge@pivotal.io>
2017-03-24 10:01:14 -0700

    Initial revision.

---
+++ nc_scripts/nc_format_cpp_files.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 <file...>" >&2
+  exit 1
+fi
+
+while [ 0 -lt $# ]
+do
+  echo "Formatting ${1}"
+  clang-format -i -style=file -fallback-style=Google ${1}
+  shift
+done
